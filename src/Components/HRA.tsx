be0ef7c045e23ea983611d927a065823ea7aab2c
palabhi017 <palabhi017@gmail.com>
2023-06-20 19:55:41 +0530

    house rent allowance calculator completed

--- src/Components/HRA.tsx
+++ src/Components/HRA.tsx
@@ -2,24 +2,24 @@ import React,{useEffect, useState} from 'react'
 import {Box, HStack, Input, VStack,Text, Checkbox, Button} from "@chakra-ui/react"
 
 interface Data{
-    BasicSalary: number,
-    DA: number,
-    Commission: number,
-    HRAReceived:number,
-    RentPaid:number,
+    BasicSalary: string,
+    DA: string,
+    Commission: string,
+    HRAReceived:string,
+    RentPaid:string,
     metrocity:boolean,
     ExemptedHRA:number,
   
 }
 
 const HRA = () => {
-    const [taxable,setTaxable] = useState(0)
+    const [taxable,setTaxable] = useState<number>(0)
     const [userData, setUserData] = useState<Data>({
-        BasicSalary: 0,
-        DA: 0,
-        Commission: 0,
-        HRAReceived:0,
-        RentPaid:0,
+        BasicSalary:'',
+        DA: '',
+        Commission: '',
+        HRAReceived:'',
+        RentPaid:'',
         metrocity:false,
         ExemptedHRA: 0,
        
@@ -43,16 +43,16 @@ const HRA = () => {
 
       const handleSubmit=()=>{
         if(userData.metrocity===true){
-            if(+userData.HRAReceived <= +(userData.BasicSalary+userData.Commission)/2 && +userData.HRAReceived <= +(userData.RentPaid-userData.BasicSalary*0.10)){
+            if(+userData.HRAReceived <= +(userData.BasicSalary+userData.Commission)/2 && +userData.HRAReceived <= +(Number(userData.RentPaid)-Number(userData.BasicSalary)*0.10)){
                 setUserData((prevUserData: Data) => ({
                     ...prevUserData,
-                     ExemptedHRA: userData.HRAReceived,
+                     ExemptedHRA: Number(userData.HRAReceived),
                     }));
                    
-            }else if(+(userData.BasicSalary+userData.Commission)/2 <= +userData.HRAReceived && +(userData.BasicSalary+userData.Commission)/2 <= +(userData.RentPaid-userData.BasicSalary*0.10) ){
+            }else if(+(userData.BasicSalary+userData.Commission)/2 <= +userData.HRAReceived && +(userData.BasicSalary+userData.Commission)/2 <= +(Number(userData.RentPaid)-Number(userData.BasicSalary)*0.10) ){
                 setUserData((prevUserData: Data) => ({
                     ...prevUserData,
-                     ExemptedHRA: (userData.BasicSalary+userData.Commission)/2,
+                     ExemptedHRA: (Number(userData.BasicSalary) + Number(userData.Commission))/2,
                      
                   }));
                
@@ -65,16 +65,16 @@ const HRA = () => {
                  
             }
         }else{
-            if(+userData.HRAReceived <= +(userData.BasicSalary+userData.Commission)*.4 && +userData.HRAReceived <= +(userData.RentPaid-userData.BasicSalary*0.10)){
+            if(+userData.HRAReceived <= +(userData.BasicSalary+userData.Commission)*.4 && +userData.HRAReceived <= +(Number(userData.RentPaid)-Number(userData.BasicSalary)*0.10)){
                 setUserData((prevUserData: Data) => ({
                     ...prevUserData,
-                     ExemptedHRA: userData.HRAReceived,
+                     ExemptedHRA: Number(userData.HRAReceived),
                     
                   }));
-            }else if(+(userData.BasicSalary+userData.Commission)*.4 <= +userData.HRAReceived && +(userData.BasicSalary+userData.Commission)*.4 <= +(userData.RentPaid-userData.BasicSalary*0.10) ){
+            }else if(+(userData.BasicSalary+userData.Commission)*.4 <= +userData.HRAReceived && +(userData.BasicSalary+userData.Commission)*.4 <= +(Number(userData.RentPaid)-Number(userData.BasicSalary)*0.10) ){
                 setUserData((prevUserData: Data) => ({
                     ...prevUserData,
-                     ExemptedHRA: (userData.BasicSalary+userData.Commission)*.4,
+                     ExemptedHRA: (Number(userData.BasicSalary)+ Number(userData.Commission))*.4,
                      
                   }));
             }else{
@@ -88,35 +88,52 @@ const HRA = () => {
         }
        
       }
+
+const handleReset=()=>{
+    setUserData({
+        BasicSalary: '',
+        DA: '',
+        Commission: '',
+        HRAReceived:'',
+        RentPaid:'',
+        metrocity:false,
+        ExemptedHRA: 0,
+       
+      })
+      setTaxable(0)
+}
   useEffect(()=>{
-    setTaxable(userData.HRAReceived-userData.ExemptedHRA)
+    setTaxable(Number(userData.HRAReceived)-userData.ExemptedHRA)
   },[userData.ExemptedHRA])
   return (
+    <>
+    
     <Box w="70vw" h={"80vh"}  m="auto" mt="5vh">
+    <Box w={"30%"} h={"auto"} p="5px" color="white" textAlign={"center"} bgColor={"#3b9ac6"} fontSize={"1.2em"}  > HOUSE RENT ALLOWANCE</Box>
       <VStack h="auto" w="100%" gap={0} border="1px solid #e6e6e6">
         <HStack w="100%" h="auto" justifyContent={"space-between"} p={"10px 20px"} bgColor={"#e6e6e6"}>
             <Text fontSize={"1.2em"}>Basic salary</Text>
-            <Input w="30%" bgColor={"white"} border={"1px solid #333"} textAlign={"right"} name='BasicSalary'  onChange={handleChange}></Input>
+            <Input w="30%" bgColor={"white"} type='number' border={"1px solid #333"} value={userData.BasicSalary} textAlign={"right"} name='BasicSalary'  onChange={handleChange}></Input>
         </HStack>
         <HStack w="100%" h="auto" justifyContent={"space-between"} p={"10px 20px"} >
             <Text fontSize={"1.2em"}>DA forming part of salary</Text>
-            <Input w="30%" bgColor={"white"} border={"1px solid #333"} textAlign={"right"} name="DA" onChange={handleChange}></Input>
+            <Input w="30%" bgColor={"white"} type='number' border={"1px solid #333"} value={userData.DA} textAlign={"right"} name="DA" onChange={handleChange}></Input>
         </HStack>
         <HStack w="100%" h="auto" justifyContent={"space-between"} p={"5px 20px"} bgColor={"#e6e6e6"}>
             <Text fontSize={"1.2em"}>Commission (as % of turnover achieved by the employee)</Text>
-            <Input w="30%" bgColor={"white"} border={"1px solid #333"} textAlign={"right"} name="Commission" onChange={handleChange}></Input>
+            <Input w="30%" bgColor={"white"} type='number' border={"1px solid #333"} value={userData.Commission} textAlign={"right"} name="Commission" onChange={handleChange}></Input>
         </HStack>
         <HStack w="100%" h="auto" justifyContent={"space-between"} p={"10px 20px"} >
             <Text fontSize={"1.2em"}>HRA Received</Text>
-            <Input w="30%" bgColor={"white"} border={"1px solid #333"} textAlign={"right"} name="HRAReceived" onChange={handleChange}></Input>
+            <Input w="30%" bgColor={"white"} type='number' border={"1px solid #333"} value={userData.HRAReceived} textAlign={"right"} name="HRAReceived" onChange={handleChange}></Input>
         </HStack>
         <HStack w="100%" h="auto" justifyContent={"space-between"} p={"10px 20px"} bgColor={"#e6e6e6"}>
             <Text fontSize={"1.2em"}>Rent Paid</Text>
-            <Input w="30%" bgColor={"white"} border={"1px solid #333"} textAlign={"right"} name="RentPaid" onChange={handleChange}></Input>
+            <Input w="30%" bgColor={"white"} type='number' border={"1px solid #333"} value={userData.RentPaid} textAlign={"right"} name="RentPaid" onChange={handleChange}></Input>
         </HStack>
         <HStack w="100%" h="auto" justifyContent={"space-between"} p={"10px 20px"} >
             <Text fontSize={"1.2em"}>Tick if residing in metro city.</Text>
-            <Text fontWeight={"bold"}><Checkbox as="span" name="metrocity" onChange={handleChange}></Checkbox>  (Tick if Yes) </Text> 
+            <Text fontWeight={"bold"}><Checkbox as="span" name="metrocity" checked={userData.metrocity} onChange={handleChange}></Checkbox>  (Tick if Yes) </Text> 
         </HStack>
         <HStack w="100%" h="auto" justifyContent={"space-between"} p={"10px 20px"} bgColor={"#e6e6e6"}>
             <Text fontSize={"1.2em"}>Exempted House Rent Allowance</Text>
@@ -129,9 +146,10 @@ const HRA = () => {
       </VStack>
       <HStack w="25%" m="auto" mt="10px">
         <Button bgColor={"tomato"} color={"white"} onClick={handleSubmit}>Calculate</Button>
-        <Button bgColor={"#999"} color={"white"}>Reset</Button>
+        <Button bgColor={"#999"} color={"white"} onClick={handleReset}>Reset</Button>
       </HStack>
     </Box>
+    </>
   )
 }
 
